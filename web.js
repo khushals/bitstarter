411e3f54005d7fdadbd6c91efdccc4691da4eebc
Khushal Shah <khushal3112@gmail.com>
2013-07-11 20:38:08 +0000

    Changes to respond()

--- web.js
+++ web.js
@@ -1,21 +1,19 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var fs = require('fs');
 
-buf =new Buffer(256);
-fs.readFileSync('$HOME/bitstarter/index.html',function(err,data){
-if(err) throw err;
-
-buf.write(data,"utf-8");
-
-});
- 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8',0,12));
+var fs = require('fs');
+var buf =new Buffer();
+response.send(buf.toString('utc-8',fs.readFileSync("index.html")));
+//fs.readFileSync('$HOME/bitstarter/index.html',function(err,data){
+//if(err) throw err;
+//buf.write(data,"utf-8");
+//});
+//response.send(buf.toString('utf-8',0,12));
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, fuction() {
+app.listen(port, fuction() { 
   console.log("Listening on " + port);
 });
