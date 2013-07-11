f9b0c0f32ee01dd4230d52049db16f1849b8afaa
Khushal Shah <khushal3112@gmail.com>
2013-07-11 19:20:41 +0000

    Changed to output buffer

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
+buf =new Buffer(256);
+fs.readFileSync('$HOME/bitstarter/index.html',function(err,data){
+if(err) throw err;
+
+buf.write(data,"utf-8");
+
+});
+ 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString('utf-8',0,12));
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+app.listen(port, fuction() {
   console.log("Listening on " + port);
 });
