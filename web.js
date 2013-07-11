e20d70ea8222c781497c11dabd8d55fbd6bdf335
Khushal Shah <khushal3112@gmail.com>
2013-07-11 20:52:04 +0000

    Check 2

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+
 app.get('/', function(request, response) {
+
 var fs = require('fs');
+
 var buf =new Buffer();
+
 response.send(buf.toString('utc-8',fs.readFileSync("index.html")));
 //fs.readFileSync('$HOME/bitstarter/index.html',function(err,data){
 //if(err) throw err;
@@ -14,5 +18,5 @@ response.send(buf.toString('utc-8',fs.readFileSync("index.html")));
 
 var port = process.env.PORT || 5000;
 app.listen(port, fuction() { 
-  console.log("Listening on " + port);
+        console.log("Listening on " + port);
 });
