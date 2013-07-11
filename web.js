c0e2a334f7cce178514bb5af262b2a755a712f03
Khushal Shah <khushal3112@gmail.com>
2013-07-11 21:29:31 +0000

    Check 2.3

--- web.js
+++ web.js
@@ -6,11 +6,7 @@ app.get('/', function(request, response) {
     var fs = require('fs');
     var buf =new Buffer(fs.readFileSynch('index.html','utf-8'));
     response.send(buf.toString());
-//fs.readFileSync('$HOME/bitstarter/index.html',function(err,data){
-//if(err) throw err;
-//buf.write(data,"utf-8");
-//});
-//response.send(buf.toString('utf-8',0,12));
+
 });
 
 var port = process.env.PORT || 5000;
