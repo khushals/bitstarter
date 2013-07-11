932ee900bb030c7210346766e93585250e56f892
Khushal Shah <khushal3112@gmail.com>
2013-07-11 22:00:38 +0000

    Removed typo error

--- web.js
+++ web.js
@@ -10,6 +10,6 @@ app.get('/', function(request, response) {
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, fuction() {
+app.listen(port, function() {
    console.log("Listening on " + port);
 });
